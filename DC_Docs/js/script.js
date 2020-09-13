55cb5362775b4ae99226ff675cb419e879d44337
terribledevice <longlost@gmail.com>
2020-09-13 09:34:05 -0400

    finished dc_docs exercise

---
+++ DC_Docs/js/script.js
@@ -0,0 +1,20 @@
+function linkColorClicks() {
+    var header = document.getElementById("navigation");
+    var btns = header.getElementsByClassName("notSel");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function() {
+            var current = document.getElementsByClassName("active");
+            if (current.length > 0) {
+                current[0].className = current[0].className.replace(" active", "");
+            }
+            this.className += " active";
+        });
+    }
+}
+
+
+// intro
+// motiv
+// job
+// campus
+// faq
